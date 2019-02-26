d992e42175192b5c192301a3bd3f76bf4b46dfe3
ibalnih <nuriqbald@gmail.com>
2019-02-26 11:25:10 +0700

    file update

--- chat.js
+++ chat.js
@@ -1,13 +1,13 @@
 function load() {
-    const chat = document.querySelector('#chat')
-    const url = 'http://localhost/ProjekKelas/chat/chat-read.php'
+    const chat = document.querySelector('#chat');
+    const url = 'http://localhost/ProjekKelas/chat/chat-read.php';
 
-    axios.get(url).then(res => {
-        chat.value = res.data;
-    })
+    axios.get(url).then(resp => {
+        chat.value = resp.data;
+    });
 
     // timeout for 1 second
     setTimeout(load, 1000);
 }
 
-load()
+load();
